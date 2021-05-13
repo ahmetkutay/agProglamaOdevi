01fb510f50224e37a983ae0b2e5761e12597de7b
Ahmet Kutay Karacair <39398963+ahmetkutay@users.noreply.github.com>
2021-05-13 21:00:51 +0300

    added broadcast messages

--- app.js
+++ app.js
@@ -1,30 +1,63 @@
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const flash = require('connect-flash');
-const session = require('express-session');
-
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const flash = require("connect-flash");
+const session = require("express-session");
+const socket = require("socket.io");
+var MongoClient = require("mongodb").MongoClient;
 const app = express();
-
+const server = app.listen(5000);
+const io = socket(server);
 // Passport Config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
-// Connect to MongoDB
+// Connect to MongoDB and socket.io
 mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true ,useUnifiedTopology: true}
-  )
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB Connected");
+
+    io.on("connection", (socket) => {
+      console.log(socket.id);
+      MongoClient.connect(db, { useUnifiedTopology: true }, function (err, db) {
+        var dbo = db.db("agProg");
+
+        let chat = dbo.collection("chats");
+        chat
+          .find({})
+          .limit(100)
+          .sort({ _id: 1 })
+          .toArray(function (err, res) {
+            if (err) {
+              throw err;
+            }
+
+            // Emit the messages
+            socket.emit("output", res);
+          });
+
+        socket.on("input", (data) => {
+          console.log("deneme");
+          let name = data.name;
+          let message = data.message;
+          console.log("data", data);
+
+          chat.insertOne({ name: name, message: message }, function () {
+            socket.emit("output", [data]);
+          });
+        });
+      });
+    });
+  })
+  .catch((err) => console.log(err));
 
 // EJS
 app.use(expressLayouts);
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Express body parser
 app.use(express.urlencoded({ extended: true }));
@@ -32,9 +65,9 @@ app.use(express.urlencoded({ extended: true }));
 // Express session
 app.use(
   session({
-    secret: 'secret',
+    secret: "secret",
     resave: true,
-    saveUninitialized: true
+    saveUninitialized: true,
   })
 );
 
@@ -46,17 +79,13 @@ app.use(passport.session());
 app.use(flash());
 
 // Global variables
-app.use(function(req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
   next();
 });
 
 // Routes
-app.use('/', require('./routes/index.js'));
-app.use('/users', require('./routes/users.js'));
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, console.log(`Server running on  ${PORT}`));
+app.use("/", require("./routes/index.js"));
+app.use("/users", require("./routes/users.js"));
